3c9d99b9656eb941f5341653565f36d88dcf524e
4skl <medi.olivier@hotmail.fr>
2023-09-29 15:15:47 +0200

    Diff functional in python
     Prototype version of the diff algorithm in Python, functional

--- lcs.py
+++ lcs.py
@@ -1,56 +1,91 @@
+# base algorithm slower version
 def lcs(input1: str, input2: str, ret_cache=False) -> str:
     """Return the longest common subsequence of a string."""
-    cache = [ ['']*(len(input1)) for _ in range(len(input2)+1) ]
-    if len(input1) == 0 or len(input2) == 0:
-        if ret_cache:
-            return '', cache
-        return ''
-    # trim beggining
-    start = 1
-    while start < len(input1) and start <= len(input2) and input1[start] == input2[start]:
-        start += 1
-    #trim ending
-    end1 = len(input1)
-    end2 = len(input2)
-    while start < end1 and start < end2 and input1[end1-1] == input2[end2-1]:
-        end1 -= 1
-        end2 -= 1
-    i1 = start
-    i2 = start
-    for i1 in range(start, end1):
-        for i2 in range(start, end2):
-            if input1[i1] == input2[i2]:
-                cache[i1-start][i2-start] = cache[i1-1-start][i2-1-start] + input1[i1]
+    cache = [ ['']*(len(input2)+1) for _ in range(len(input1)+1) ]
+    i = 0
+    j = 0
+    for i in range(1, len(input1)+1):
+        for j in range(1, len(input2)+1):
+            if input1[i-1] == input2[j-1]:
+                cache[i][j] = cache[i-1][j-1] + input1[i-1]
             else:
-                cache[i1-start][i2-start] = max(cache[i1-start][i2-1-start], cache[i1-1-start][i2-start], key=len)
-    print(i1, i2, len(cache), len(cache[0]))
+                cache[i][j] = max(cache[i][j-1], cache[i-1][j], key=len)
     if ret_cache:
-        return cache[i1-start-1][i2-start-1], cache
-    return cache[i1-start-1][i2-start-1]
-    
-def print_diff(cache, input1, input2, i=None, j=None):
-    print(cache)
+        return cache[i][j], cache
+    return cache[i][j]
+
+# base algorithm faster version
+def lcsm(input1: str, input2: str, ret_cache=False) -> str:
+    start = 1
+    i_end = len(input1) # m = 
+    j_end = len(input2) # n =
+    # trim off the matching items at the beginning
+    while start <= i_end and start <= j_end and input1[start-1] == input2[start-1]:
+        start += 1
+    # trim off the matching items at the end
+    while start <= i_end and start <= j_end and input1[i_end-1] == input2[j_end-1]:
+        i_end -= 1
+        j_end -= 1
+    if not ret_cache:
+        return input1[:start-1] + lcs(input1[start-1:i_end], input2[start-1:j_end]) + input1[i_end:j_end]
+    else:
+        lcs_out, cache = lcs(input1[start-1:i_end], input2[start-1:j_end], ret_cache=True)
+        return input1[:start-1] + lcs_out + input1[i_end:], cache
+
+# fast algoritm creating output for diff
+def lcs_diff(input1: str, input2: str) -> str:
+    start = 1
+    i_end = len(input1) # m = 
+    j_end = len(input2) # n =
+    # trim off the matching items at the beginning
+    while start <= i_end and start <= j_end and input1[start-1] == input2[start-1]:
+        start += 1
+    # trim off the matching items at the end
+    while start <= i_end and start <= j_end and input1[i_end-1] == input2[j_end-1]:
+        i_end -= 1
+        j_end -= 1
+    lcs_out, cache = lcs(input1[start-1:i_end], input2[start-1:j_end], ret_cache=True)
+    return input1[:start-1] + lcs_out + input1[i_end:], cache, start, i_end, j_end
+
+# computing and printnig the diff
+def print_diff(input1, input2, cache=None, i=None, j=None, start=None, i_end=None, j_end=None):
+    t_input1 = input1
+    t_input2 = input2
+    f_it = False
+    if cache is None:
+        _, cache, start, i_end, j_end = lcs_diff(input1, input2)
+        for c in input1[:start-1]:
+            print(" ", c)
+        t_input1 = input1[start-1:i_end]
+        t_input2 = input2[start-1:j_end]
+        f_it = True
     if i is None:
-        i = len(cache)
+        i = len(t_input1)
     if j is None:
-        j = len(cache[0])
-    if i >= 0 and j >= 0 and input1[i] == input2[j]:
-        print_diff(cache, input1, input2, i-1, j-1)
-        print("  " + input1[i])
+        j = len(t_input2)
+    if i > 0 and j > 0 and t_input1[i-1] == t_input2[j-1]:
+        print_diff(t_input1, t_input2, cache=cache, i=i-1, j=j-1)
+        print(" ", t_input1[i-1])
     elif j > 0 and (i == 0 or len(cache[i][j-1]) >= len(cache[i-1][j])):
-        print_diff(cache, input1, input2, i, j-1)
-        print("+ " + input2[j])
+        print_diff(t_input1, t_input2, cache=cache, i=i, j=j-1)
+        print("+",t_input2[j-1])
     elif i > 0 and (j == 0 or len(cache[i][j-1]) < len(cache[i-1][j])):
-        print_diff(cache, input1, input2, i-1, j)
-        print("- " + input1[i])
+        print_diff(t_input1, t_input2, cache=cache, i=i-1, j=j)
+        print("-", t_input1[i-1])
     else:
         print("")
+    if f_it:
+        for c in input1[i_end:]:
+            print(" ", c)
 
-def fucked_up_diff_on_file(filename1, filename2):
+def diff_on_file(filename1, filename2):
     text1 = open(filename1).read()
     text2 = open(filename2).read()
     sol, cache = lcs(text1, text2, ret_cache=True)
     print(sol)
     print_diff(cache, text1, text2)
 
-fudof = fucked_up_diff_on_file
+fudof = diff_on_file
+text1 = "BAZERTY"
+text2 = "AZDFTY"
+print_diff(text1, text2)
